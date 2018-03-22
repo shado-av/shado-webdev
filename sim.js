ac27661fef8cad4d559604d6302ee6aa44f15624
Alina Walling <alina.walling@duke.edu>
2018-03-22 04:07:48 -0400

    Simulator design and forms updated, Vue.js reactive backend implemented

--- sim.js
+++ sim.js
@@ -7,21 +7,116 @@ var sim = new Vue({
             simType: "railroad",
             numHours: 8,
             diffTrafficLevels: "n",
-            trafficLevels: ["m", "m", "m", "m", "m", "m", "m", "m"]
+            trafficLevels: ["m", "m", "m", "m", "m", "m", "m", "m"],
+            exoFactors: 0,
+            exoFactorsType: 1
         },
         taskSettings: {
             numPhases: 2,
-            tasks: []
+            tasks: 
+            [
+                {
+                    name: "Communicating",
+                    include: false,
+                    isCustom: false,
+                    priority: [4, 7],
+                    arrivalDistribution: "E",
+                    arrivalParam: [0.033333,0.1],
+                    serviceDistribution: "U",
+                    serviceParam: [0.5,2],
+                    expireDistribution: "E",
+                    expireParamDefault: [0,0.184],
+                    expireParamExo: [0,0.184],
+                    affectedByTraffic: "y",
+                    affectByIROPS: [0,1],
+                    humanErrorProb: [0.0004,0.00008,0.007]
+                },
+                {
+                    name: "Actuation",
+                    include: false,
+                    isCustom: false,
+                    priority: [5,5],
+                    arrivalDistribution: "E",
+                    arrivalParam: [0.033333,0.1],
+                    serviceDistribution: "U",
+                    serviceParam: [0.5,2],
+                    expireDistribution: "E",
+                    expireParamDefault: [0,0.184],
+                    expireParamExo: [0,0.184],
+                    affectedByTraffic: "y",
+                    affectByIROPS: [0,1],
+                    humanErrorProb:[0.0004,0.00008,0.007]
+                },
+                {
+                    name: "Directive Mandatory",
+                    include: false,
+                    isCustom: false,
+                    priority: [5,5],
+                    arrivalDistribution: "E",
+                    arrivalParam: [0.033333,0.1],
+                    serviceDistribution: "U",
+                    serviceParam: [0.5,2],
+                    expireDistribution: "E",
+                    expireParamDefault: [0,0.184],
+                    expireParamExo: [0,0.184],
+                    affectedByTraffic: "y",
+                    affectByIROPS: [0,1],
+                    humanErrorProb: [0.0004,0.00008,0.007]
+                }
+            ]
         },
         operatorSettings: {
-            numTeams: 1,
-            teams: []
+            numTeams: 2,
+            teams: 
+            [
+                {
+                    name: "Operator Team",
+                    size: 1,
+                    strategy: "FIFO",
+                    comms: "N",
+                    tasks: [],
+                    AIDA: {
+                        equalOperator: false,
+                        assistingIndividuals: "N",
+                        assistingTeamCoord: "N"  
+                    },
+                    failTresh: 50
+                },
+                {
+                    name: "Operator Team",
+                    size: 1,
+                    strategy: "FIFO",
+                    comms: "N",
+                    tasks: [],
+                    AIDA: {
+                        equalOperator: false,
+                        assistingIndividuals: "N",
+                        assistingTeamCoord: "N"  
+                    },
+                    failTresh: 50
+                } 
+            ]
         },
         fleetSettings: {
             numFleets: 1,
-            fleets: []
+            fleets: 
+            [
+                {
+                    name: "Fleet",
+                    numVehicles: 1,
+                    comms: "N",
+                    tasks: []
+                } 
+            ]
         },
         numReps: 1
+    },
+    
+    computed: {
+        
+        maxTasksReached () {
+            return this.getNumIncludedTasks() >= 15;
+        }
     },
     
     methods: {
@@ -41,6 +136,107 @@ var sim = new Vue({
             } else {
                 lvls.splice(this.globalSettings.numHours);
             }
+        },
+        
+        getNumIncludedTasks () {
+            var included = 0;
+            for (i = 0; i < this.taskSettings.tasks.length; i++) {
+                if (this.taskSettings.tasks[i].include) {
+                    included++;
+                }
+            }
+            return included;
+        },
+        
+        disableAddTask (task) {
+            if (task.include || !this.maxTasksReached) {
+                return false;
+            } 
+            return true;
+        },
+        
+        addCustomTask () {
+            this.taskSettings.tasks.push({
+                name: "Custom Task",
+                include: true,
+                isCustom: true,
+                priority: [],
+                arrivalDistribution: "E",
+                arrivalParam: [],
+                serviceDistribution: "E",
+                serviceParam: [],
+                expireDistribution: "E",
+                expireParamDefault: [],
+                expireParamExo: [],
+                affectedByTraffic: "n",
+                affectByIROPS: [],
+                humanErrorProb: []
+            })
+        },
+        
+        removeCustomTask (task) {
+            if (confirm("Are you sure you want to delete this custom task?")) {
+                this.taskSettings.tasks.splice(this.taskSettings.tasks.indexOf(task), 1); 
+            }
+        },
+        
+        isAffectedByTrafficPhases (task) {
+            return task.affectedByTraffic === "y" && this.taskSettings.numPhases > 1;
+        },
+        
+        updateOperatorTeams () {
+            var teams = this.operatorSettings.teams;
+            if (this.operatorSettings.numTeams > teams.length) {
+                while (teams.length < this.operatorSettings.numTeams) {
+                    teams.push({
+                        name: "Operator Team",
+                        size: 1,
+                        strategy: "FIFO",
+                        comms: "N",
+                        tasks: [],
+                        AIDA: {
+                            equalOperator: false,
+                            assistingIndividuals: "N",
+                            assistingTeamCoord: "N"  
+                        },
+                        failTresh: 50
+                    })
+                }
+            } else {
+                teams.splice(this.operatorSettings.numTeams);
+            }
+        },
+        
+        removeOperatorTeam (team) {
+            if (confirm("Are you sure you want to delete this operator team?")) {
+                this.operatorSettings.teams.splice(this.operatorSettings.teams.indexOf(team), 1); 
+            }
+            this.operatorSettings.numTeams = this.operatorSettings.teams.length;
+        },
+        
+        updateFleets () {
+            var fleets = this.fleetSettings.fleets;
+            if (this.fleetSettings.numFleets > fleets.length) {
+                while (fleets.length < this.fleetSettings.numFleets) {
+                    fleets.push({
+                        name: "Fleet",
+                        numVehicles: 1,
+                        comms: "N",
+                        tasks: []
+                    })
+                }
+            } else {
+                fleets.splice(this.fleetSettings.numFleets);
+            }
+            
+        },
+        
+        removeFleet (fleet) {
+            if (confirm("Are you sure you want to delete this fleet?")) {
+                this.fleetSettings.fleets.splice(this.fleetSettings.fleets.indexOf(fleet), 1); 
+            }
+            this.fleetSettings.numFleets = this.fleetSettings.fleets.length;
+            
         }
     }
     
