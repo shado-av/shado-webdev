edba1342c1d48f1e1dfa9bb539160b13a4b7e455
Hanwiz <hanwiz@gmail.com>
2018-11-28 00:13:07 -0500

    update server url to http

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 var env = {
-    serverUrl: "https://apps.hal.pratt.duke.edu:8080",
+    serverUrl: "http://apps.hal.pratt.duke.edu:8080",
     //serverUrl: "http://localhost:8080",
     production : true
 };
